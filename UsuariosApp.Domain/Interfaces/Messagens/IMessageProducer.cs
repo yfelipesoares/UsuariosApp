0e86d9bbc0f46b609b6d5a46bffc56cd3cbebaa9
yfelipesoares <yfelipe.soares@gmail.com>
2024-10-10 19:27:39 -0300

    first commit

---
+++ UsuariosApp.Domain/Interfaces/Messagens/IMessageProducer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsuariosApp.Domain.Interfaces.Messagens
+{
+    /// <summary>
+    /// Interface para definir os métodos que irão gravar conteudo
+    /// no servidor da mensageria
+    /// </summary>
+    public interface IMessageProducer
+    {
+        /// <summary>
+        /// Método para enviarmos uma mensagem na fila do servidor da mensageria
+        /// </summary>
+        /// <param name="message"></param>
+        void Send(string message);
+    }
+}
